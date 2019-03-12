61b5bc2b8e1b2597a7395e42bd6b293361b33c38
Thomas Luk <thomas16@uw.edu>
2019-03-11 19:06:03 -0700

    wrangle

--- app.R
+++ app.R
@@ -5,6 +5,12 @@
 library(shiny)
 library(dplyr)
 
+education_by_county <- read.csv("./data/education_by_county.csv", stringsAsFactors = FALSE)
+colnames(education_by_county) <- c("FIPS.Code", "state", "area_name", "less_than_high", "high_only", "some_college", "bachelors", "per_less_than_high", "per_high_only", "per_some_college", "per_bachelors")
+census_county_data <- read.csv("./data/census_county_data.csv", stringsAsFactors = FALSE)
+joined <- inner_join(education_by_county, census_county_data, by="FIPS.Code")
+joined <- mutate(joined, Non_White = 100 - White)
+
 # data sets
 census_data <- read.csv("./data/census_county_data.csv")
 education_data <- read.csv("./data/education_by_county.csv")
@@ -13,7 +19,7 @@ education_data <- read.csv("./data/education_by_county.csv")
 # UI
 ad5_ui <- navbarPage("AD5 Final",
      tabPanel("Introduction"),
-     tabPanel("Question One"),
+     tabPanel("Question One", tabPanel("bachelors_income", titlePanel("Data for Individual Countries"))), 
      tabPanel("Question Two"),
      tabPanel("Question Three"),
      tabPanel("Question Four"),
@@ -22,6 +28,12 @@ ad5_ui <- navbarPage("AD5 Final",
 
 # Server
 ad5_server <- function(input, output){
+  output$question_one <- renderPlot({
+    bachelors_income <- ggplot(data = joined) +
+      geom_hex(mapping = aes(x = per_bachelors, y = IncomePerCap)) +
+      labs(title = "Prominence of Bachelors Degrees and Income per Capita", x ="% Population with Bachelors Degree or Higher", y = "Income Per Capita")
+    bachelors_income
+  })
   
 }
 
