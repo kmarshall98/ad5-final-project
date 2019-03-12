5af5042d1c0f5c04b3a0256ae334a68641eef9f4
Thomas Luk <thomas16@uw.edu>
2019-03-11 19:21:22 -0700

    Add graph for question one

--- app.R
+++ app.R
@@ -1,14 +1,17 @@
-# AD5 Final Project 
+# AD5 Final Project
 # Kendall Marshall, James Hovet, Thomas Luk, and Dan Palanchuk
 
 # Necessary Packages
-library(shiny)
-library(dplyr)
+library("shiny")
+library("dplyr")
+library("ggplot2")
+library("tidyr")
+library("rsconnect")
 
 education_by_county <- read.csv("./data/education_by_county.csv", stringsAsFactors = FALSE)
 colnames(education_by_county) <- c("FIPS.Code", "state", "area_name", "less_than_high", "high_only", "some_college", "bachelors", "per_less_than_high", "per_high_only", "per_some_college", "per_bachelors")
 census_county_data <- read.csv("./data/census_county_data.csv", stringsAsFactors = FALSE)
-joined <- inner_join(education_by_county, census_county_data, by="FIPS.Code")
+joined <- inner_join(education_by_county, census_county_data, by = "FIPS.Code")
 joined <- mutate(joined, Non_White = 100 - White)
 
 # data sets
@@ -17,25 +20,38 @@ education_data <- read.csv("./data/education_by_county.csv")
 
 
 # UI
-ad5_ui <- navbarPage("AD5 Final",
-     tabPanel("Introduction"),
-     tabPanel("Question One", tabPanel("bachelors_income", titlePanel("Data for Individual Countries"))), 
-     tabPanel("Question Two"),
-     tabPanel("Question Three"),
-     tabPanel("Question Four"),
-     tabPanel("References")
+ad5_ui <- navbarPage(
+  "AD5 Final",
+  tabPanel("Introduction"),
+  tabPanel(
+    "Question One",
+    titlePanel(""),
+    sidebarLayout(
+      sidebarPanel(
+        titlePanel("Select Data")
+      ),     
+      mainPanel(
+        titlePanel("Prominence of Bachelors Degrees and Income per Capita"),
+          plotOutput("question_one"),
+          p("Hello world!")
+      )
+    )
+  ),
+  tabPanel("Question Two"),
+  tabPanel("Question Three"),
+  tabPanel("Question Four"),
+  tabPanel("References")
 )
 
 # Server
-ad5_server <- function(input, output){
+ad5_server <- function(input, output) {
   output$question_one <- renderPlot({
     bachelors_income <- ggplot(data = joined) +
       geom_hex(mapping = aes(x = per_bachelors, y = IncomePerCap)) +
-      labs(title = "Prominence of Bachelors Degrees and Income per Capita", x ="% Population with Bachelors Degree or Higher", y = "Income Per Capita")
+      labs(title = "Prominence of Bachelors Degrees and Income per Capita", x = "% Population with Bachelors Degree or Higher", y = "Income Per Capita")
     bachelors_income
   })
-  
 }
 
-# Shiny App 
+# Shiny App
 shinyApp(ui = ad5_ui, server = ad5_server)
