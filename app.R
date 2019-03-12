5426211af374e325d1d83bebad12eb00af5e5893
Kendall Marshall <marshk7@uw.edu>
2019-03-11 18:42:45 -0700

    Reformatted App

--- app.R
+++ app.R
@@ -11,31 +11,13 @@ education_data <- read.csv("./data/education_by_county.csv")
 
 
 # UI
-ad5_ui <- fluidPage(
-  # app title
-  titlePanel("Analysis Report on the relationship between Education and Success"),
-  p("AD5 Final Project by Kendall Marshall, James Hovet, Thomas Luk, Dan Palanchuck"),
-  p("3/11/2019"),
-  h4("Introduction"),
-  p("INTRODUCTION OF WHAT THE DATA REPORT IS ANALYZING.......afjsdkfjFHJGSDAKJDFHDSFJOekw
-    qehefjkffjaskfsakfnsakjfsjnafndsajfnsdjkfnsdkjfn"),
-  sidebarLayout(
-    
-    sidebarPanel(
-       
-    ),
-    
-    mainPanel(
-      tabsetPanel(
-        tabPanel("question_one", plotOutput("person_one_plot"), textOutput(outputId = "question_one_info")),
-        tabPanel("question_two", plotOutput("person_two_plot"), textOutput(outputId = "question_two_info")),
-        tabPanel("question_three", plotOutput("person_three_plot"), textOutput(outputId = "question_three_info")),
-        tabPanel("question_four", plotOutput("person_four_plot"), textOutput(outputId = "question_four_info"))
-        )
-        
-    )
-  ),
-  h4("References:")
+ad5_ui <- navbarPage("AD5 Final",
+     tabPanel("Introduction"),
+     tabPanel("Question One"),
+     tabPanel("Question Two"),
+     tabPanel("Question Three"),
+     tabPanel("Question Four"),
+     tabPanel("References")
 )
 
 # Server
